6854ef9d4488ba392a3ef4a740c837baca6d9917
arpit551 <imarpit03@gmail.com>
2018-10-02 19:39:11 +0530

    django

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+	return render(request,'room/index.html')
+def form(request):
+	return render(request,'room/book_a_room.html')
