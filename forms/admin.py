88abc73996768ff8b7f6bac5bfc0d5f2cf843a94
arpit551 <imarpit03@gmail.com>
2018-10-06 13:13:18 +0530

    django

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import FormSubmit
+
+admin.site.site_header = "MDP ADMIN PAGE";
+admin.site.site_title = "ADMISTRATOR";
+
 
 # Register your models here.
+class AllEntiryAdmin(admin.ModelAdmin):
+    list_display = ("name", "id")
+admin.site.register(FormSubmit,AllEntiryAdmin)   
